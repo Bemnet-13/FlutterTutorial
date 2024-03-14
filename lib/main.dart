a358ed811e2dd69891b9d94b9e45e86b020b963b
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-03-14 10:03:55 +0300

    Added asset image

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://cdn.shopify.com/s/files/1/1045/2442/products/Iamverypoor_shopify01_1024x1024.jpg?v=1593527267'),
+            image: AssetImage('images/iamverypoor.webp'),
           ),
         ),
       ),
